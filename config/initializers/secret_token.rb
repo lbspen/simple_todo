e7aa31f6109ff091b6fd4c20cccc5897a4b036fb
lbs <lbspen@gmail.com>
2013-06-01 18:42:50 -0700

    initial generation of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTodo::Application.config.secret_token = '5d0659b406914fb89e5f0cb15c87af0a436f96992382e0b8ed81e06cf03bc1246e721371887937f3bb5f83c25240357a615440cd74bdecb0c4d2cefc830aa2fd'
